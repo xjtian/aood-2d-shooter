a3eeaf2319579b830ce63136903bf3ebae09b0c3
xtian8741 <xtian8741@OMH4040DTDE030>
2012-04-13 08:08:52 -0400

    Some baseclasses

---
+++ src/baseclasses/Player.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package baseclasses;
+
+/**
+ *
+ * @author xtian8741
+ */
+public class Player {
+    protected int direction;
+    protected int x;
+    protected int y;
+    
+}
---
+++ src/baseclasses/PlayerSprite.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package baseclasses;
+
+/**
+ *
+ * @author xtian8741
+ */
+public enum PlayerSprite {
+    
+}
